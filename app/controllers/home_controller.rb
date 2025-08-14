37ba7700c4688939246c493459e60cba3331c32a
issei <proteinebimayoduu@gmail.com>
2025-08-15 07:03:01 +0900

    ・画面幅に応じてボードの表示件数を変える ・ページャの設置

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,17 +1,28 @@
 class HomeController < ApplicationController
   def index
+    # URLパラメータが無ければ5をデフォルトに
+    per_page = params[:per_page].presence&.to_i || 5
+    per_page = 5 if per_page <= 0
+
+    @page = params[:page].to_i
+    @page = 1 if @page < 1
+
     @latest_boards =
-  Board
-    .includes(
-      user:  [ avatar_attachment: :blob ],
-      tasks: [
-        :comments,
-        :user,                    # タスク投稿者
-        { eyecatch_attachment: :blob },         # タスクの画像があるなら
-        { user: [ avatar_attachment: :blob ] }  # タスク投稿者のアバター
-      ]
-    )
-    .order(created_at: :desc)
-    .limit(3)
+      Board
+        .includes(
+          user:  [ avatar_attachment: :blob ],
+          tasks: [
+            :user,
+            { comments: :user },
+            { eyecatch_attachment: :blob },
+            { user: [ avatar_attachment: :blob ] }
+          ]
+        )
+        .order(created_at: :desc)
+        .offset((@page - 1) * per_page)
+        .limit(per_page)
+
+    @total_pages = (Board.count / per_page.to_f).ceil
+    @per_page    = per_page
   end
 end
