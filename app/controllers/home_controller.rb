85d1efaf49ea9f635663dce0ff6d670217d65a2b
issei <proteinebimayoduu@gmail.com>
2025-08-14 15:28:31 +0900

    トップページのカード表示調整（タイトル下に概要を表示） ボード内にタスクカードが縦並びに続くように設定中。

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,16 @@
 class HomeController < ApplicationController
   def index
     @latest_boards =
-      Board
-        .includes(user: { avatar_attachment: :blob })
-        .order(created_at: :desc)
-        .limit(3)
+  Board
+    .includes(
+      user:  [ avatar_attachment: :blob ],
+      tasks: [
+        :user,                    # タスク投稿者
+        { eyecatch_attachment: :blob },         # タスクの画像があるなら
+        { user: [ avatar_attachment: :blob ] }  # タスク投稿者のアバター
+      ]
+    )
+    .order(created_at: :desc)
+    .limit(3)
   end
 end
