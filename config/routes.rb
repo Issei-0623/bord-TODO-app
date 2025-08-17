21d1c744d5c3b7a45a0a54751303cfe6608d9d5a
issei <proteinebimayoduu@gmail.com>
2025-08-18 00:28:01 +0900

    ログイン機能をdeviseで実装し直す。※セキュリティ面等を考慮して、デザインはこれから微調整する

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: "home#index"
-  
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'signup' }
+
+  devise_scope :user do
+    get '/users/auth_cards', to: redirect('/login')
+    get '/auth_cards',       to: redirect('/login')
+  end
+
+  root to: 'home#index'
+
   resources :boards do
     resources :tasks
   end
@@ -10,10 +16,8 @@ Rails.application.routes.draw do
     resources :comments, only: [:new, :create, :edit, :update, :destroy]
   end
 
+  resources :comments, only: [:new, :create, :edit, :update, :destroy]
+
   resource :profile, only: [:show, :edit, :update]
-
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  get "users/auth_cards", to: "users#auth_cards"
 
 end
