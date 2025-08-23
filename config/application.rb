9b3e5e7eb922abf79f44eea79d98595e6eccee43
issei <proteinebimayoduu@gmail.com>
2025-08-23 20:31:50 +0900

    文法ミス（endの付け忘れ）の修正

--- config/application.rb
+++ config/application.rb
@@ -16,27 +16,28 @@ module BordTodoApp
     config.load_defaults 8.0
 
     if Rails.env.development? || Rails.env.test?
-    Bundler.require(*Rails.groups)
-    Dotenv::Railtie.load
+      Bundler.require(*Rails.groups)
+      Dotenv::Railtie.load
 
-    # 表示用のタイムゾーン（ビューや Time.current がこれになる）
-    config.time_zone = 'Asia/Tokyo'  # または 'Tokyo'
+      # 表示用のタイムゾーン（ビューや Time.current がこれになる）
+      config.time_zone = 'Asia/Tokyo'  # または 'Tokyo'
 
-    # DB への保存は UTC のままが安全（推奨）
-    config.active_record.default_timezone = :utc
+      # DB への保存は UTC のままが安全（推奨）
+      config.active_record.default_timezone = :utc
 
-    # Please, add to the `ignore` list any other `lib` subdirectories that do
-    # not contain `.rb` files, or that should not be reloaded or eager loaded.
-    # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w[assets tasks])
+      # Please, add to the `ignore` list any other `lib` subdirectories that do
+      # not contain `.rb` files, or that should not be reloaded or eager loaded.
+      # Common ones are `templates`, `generators`, or `middleware`, for example.
+      config.autoload_lib(ignore: %w[assets tasks])
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
-    config.i18n.default_locale = :ja
+      # Configuration for the application, engines, and railties goes here.
+      #
+      # These settings can be overridden in specific environments using the files
+      # in config/environments, which are processed later.
+      #
+      # config.time_zone = "Central Time (US & Canada)"
+      # config.eager_load_paths << Rails.root.join("extras")
+      config.i18n.default_locale = :ja
+    end
   end
 end
